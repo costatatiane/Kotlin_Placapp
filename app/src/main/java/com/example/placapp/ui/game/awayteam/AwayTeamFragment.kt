6959a56f69199fcf89339c2b0ea9294827cabcd9
Logon Aluno <logonaluno@local.com>
2020-02-08 11:46:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/placapp/ui/game/awayteam/AwayTeamFragment.kt
@@ -0,0 +1,46 @@
+package com.example.placapp.ui.game.awayteam
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.placapp.R
+
+import com.example.placapp.ui.score.ScoreActivity
+import kotlinx.android.synthetic.main.fragment_away_team.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AwayTeamFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_away_team, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btNextStep.setOnClickListener {
+            sendHomeTeamName()
+        }
+    }
+    private fun sendHomeTeamName() {
+        val intent = Intent("FILTER_AWAY_TEAM")
+        intent.putExtra("away_team", inputAwayTeam.text.toString())
+        LocalBroadcastManager.getInstance(requireContext()).sendBroadcast(intent)
+    }
+
+}
